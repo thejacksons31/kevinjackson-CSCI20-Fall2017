d462e5176dd27134837c412d35903f0548628e68
Kevin Jackson <kevinpjackson13@yahoo.com>
2017-08-23 19:24:24 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,6 @@
+/*determine what two numbers need to be choosen from
+make the program choose randomly between the two numbers
+wait till the user imputs a guess to witch number he thinks it is
+If user chooses the right number then the user wins
+if user chooses the wrong number then the wrong number then the program tells the user higher or lower till right number choosen
+if user chooses the wrong number then the program minuses one point from 10./*
