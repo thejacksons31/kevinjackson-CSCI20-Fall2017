ca8f60408052395ead31a18936e1d3ae548b7747
Kevin Jackson <kevinpjackson13@yahoo.com>
2017-08-28 19:14:50 +0000

    Lab12 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,19 @@
+/* Created by: Kevin Jackson
+Discription: This program will output a face
+Created: 8/28/2017*/
+
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout<<"   _"<<"_____"<<endl;
+     cout<<" / /"<<"\\ /\\ \\"<<endl;
+     cout<<"/  \\/ \\/  \\"<<endl;
+     cout<<"|    <    |"<<endl;
+     cout<<" \\       /"<<endl;
+     cout<<"  \\ ~~ /"<<endl;
+     cout<<"    __"<<endl;
+     
+ }
+ 
